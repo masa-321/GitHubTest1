34aa2af0699da3abc75d858163212f1df995cfa5
masa-321 <ma.ly321.luna.notte@gmail.com>
2019-06-21 07:15:02 +0900

    initial commit

---
+++ GitHubTest1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubTest1
+//
+//  Created by Masahiro Atarashi on 2019/06/21.
+//  Copyright © 2019 Masahiro Atarashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
