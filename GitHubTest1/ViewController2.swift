490aebe3ea234c13afcdd9aea4750a5ac3988c47
masa-321 <ma.ly321.luna.notte@gmail.com>
2019-06-22 06:41:22 +0900

    ViewController2を追加した。

--- GitHubTest1/ViewController2.swift
+++ GitHubTest1/ViewController2.swift
@@ -1,20 +1,19 @@
 //
-//  ViewController.swift
+//  ViewController2.swift
 //  GithubTest1
 //
-//  Created by Masahiro Atarashi on 2019/06/21.
+//  Created by Masahiro Atarashi on 2019/06/22.
 //  Copyright © 2019 Masahiro Atarashi. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController2: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     
 }
-
